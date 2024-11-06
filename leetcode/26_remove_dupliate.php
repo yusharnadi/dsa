953f840ae6bd025c2534f10d8f53401ac129d06b
yusharnadi <yust.44@gmail.com>
2024-11-06 21:41:33 +0700

    Challenge done 26

--- leetcode/26_remove_dupliate.php
+++ leetcode/26_remove_dupliate.php
@@ -1,1 +1,22 @@
 <?php
+
+/**
+ * @param Integer[] $nums
+ * @return Integer
+ */
+function removeDuplicates(array &$nums) :int {
+    $h = [];
+
+    for ($i = 0; $i < count($nums); $i++) {
+        $h[$nums[$i]] = $nums[$i];
+    }
+
+    $nums = $h;
+
+    return count($nums);
+}
+
+$nums = [0,0,1,1,1,2,2,3,3,4];
+
+echo removeDuplicates($nums) .PHP_EOL;
+print_r($nums);
